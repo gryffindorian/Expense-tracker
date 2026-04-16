1133bba49f25ceec8e0d358eb1443628063a2aed
Ananya Jain <ananyaj000@gmail.com>
2026-04-17 01:43:41 +0530

    added reisterUser impl

---
+++ expense-tracker/src/main/java/com/ananya/jain/expense_tracker/service/AuthService.java
@@ -0,0 +1,55 @@
+package com.ananya.jain.expense_tracker.service;
+
+import com.ananya.jain.expense_tracker.dto.AuthResponse;
+import com.ananya.jain.expense_tracker.dto.LoginRequest;
+import com.ananya.jain.expense_tracker.dto.RegisterRequest;
+import com.ananya.jain.expense_tracker.entity.User;
+import com.ananya.jain.expense_tracker.repository.UserRepository;
+import com.ananya.jain.expense_tracker.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthResponse register(RegisterRequest registerRequest){
+
+        if(userRepository.existsByEmail(registerRequest.getEmail())){
+            throw new RuntimeException("User already exists");
+        }
+
+        else {
+            String hashedPassword = passwordEncoder.encode(registerRequest.getPassword());
+            User user = User.builder()
+                    .password(hashedPassword)
+                    .email(registerRequest.getEmail())
+                    .name(registerRequest.getName())
+                    .build();
+
+            userRepository.save(user);
+
+            String jwtToken = jwtService.generateToken(registerRequest.getEmail());
+
+            return AuthResponse.builder()
+                    .jwtToken(jwtToken)
+                    .build();
+        }
+    }
+
+//    public AuthResponse login(LoginRequest loginRequest){
+//        if(!userRepository.existsByEmail(loginRequest.getEmail())){
+//            throw new RuntimeException("No user exists");
+//        }
+//        else {
+//            String hashedPassword = passwordEncoder.encode(loginRequest.getPassword());
+//
+//            if(hashedPassword.equals(userRepository.findByEmail(loginRequest.getEmail()).getPassword))
+//        }
+//    }
+}
